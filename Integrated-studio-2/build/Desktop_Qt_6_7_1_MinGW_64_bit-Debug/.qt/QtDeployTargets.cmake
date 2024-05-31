ee22cd8208e2dfedd1ccdf2ca6592628654a6d25
Bernadette Wilson <Bernadettewilson60@gmail.com>
2024-05-31 12:59:36 +1200

    fix

---
+++ Integrated-studio-2/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Library-system_FILE R:/QT projects/Library-system/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Library-system.exe)
+set(__QT_DEPLOY_TARGET_Library-system_RUNTIME_DLLS R:/QT/6.7.1/mingw_64/bin/Qt6Widgets.dll;R:/QT/6.7.1/mingw_64/bin/Qt6Gui.dll;R:/QT/6.7.1/mingw_64/bin/Qt6Core.dll)
