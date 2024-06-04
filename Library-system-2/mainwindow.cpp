efeaff861789c241cb4cf948682adb48e0e76729
Braedan M <270336855@yoobeestudent.ac.nz>
2024-06-04 15:55:07 +1200

    Added new page, pg edits, btn to change page, resource flder

--- Library-system-2/mainwindow.cpp
+++ Library-system-2/mainwindow.cpp
@@ -2,20 +2,28 @@
 #include "./ui_mainwindow.h"
 
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->pushButton_admin, SIGNAL(clicked()), this,SLOT(page1Widget()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_pushButton_clicked()
+//switches pages
+void MainWindow::on_pushButton_admin_clicked()
 {
-    //
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_bckhome_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
