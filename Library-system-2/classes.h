8fc464170c519dcbe73a351b0a9e1d72817cda24
Braedan M <270336855@yoobeestudent.ac.nz>
2024-06-05 14:18:27 +1200

    start of classes with new header file

---
+++ Library-system-2/classes.h
@@ -0,0 +1,15 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+#include <Vector>
+
+class Library{
+private:
+    //User: user;
+    //Book: book;
+    //std::vector<Book> catalogue;
+
+   // void addUser(name, address) {}
+    // void addBook(bookName, authorName)
+};
+
+#endif // CLASSES_H
